7a817e379031e2f36ddab6ef6c521bde1cb33176
fs1989 <farseer1989@outlook.com>
2015-10-15 21:47:12 +0800

    Home edtion

--- JobCenter/src/com/fs/Window.java
+++ JobCenter/src/com/fs/Window.java
@@ -1,25 +1,37 @@
 package com.fs;
 
-import org.eclipse.jface.dialogs.MessageDialog;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
 public class Window {
-
+	
+	Application petrel = new Application("petrel", "FGG-GLOBAL-ZZ-SCHL-Shell_Petrel-GS");
+	Application techlog = new Application("techlog", "FGG-EP-GLOBAL-ZZ-THSA-Techlog-AllVersions-GS");
+	Application arcgis = new Application("arcgis", "FGG-GLOBAL-ZZ-ARCGIS-GS");
+	Application fme = new Application("fme", "FGR-SCEPCO-BEJ-ZZ-SAFE-FME-2013_SP3-GS");
+	Application ofm = new Application("ofm", "FGG-GLOBAL-ZZ-SCHL-OFM-GS");
+	Application edm = new Application("edm", "FGO-SUKEP-ABE-ZZ-EDM R5000 SCEPCO-GS");
+	Application ecrin = new Application("ecrin", "FGG-EP-GLOBAL-ZZ-KAEN-Ecrin-GS");
+	
 	public static void main(String[] args) {  
+		
         Display display = new Display();  
-        //1.一个窗口  
-        final Shell shell = new Shell(display);  
-        shell.setSize(250, 300);  
-        shell.setText("Hello SWT");  
+        Shell shell = new Shell(display);  
+        shell.setSize(1024, 600);  
+        shell.setLayout(new FillLayout());
+        shell.setText("SSW Application Support");
+        
         //2.一个按钮  
-        Button button = new Button(shell, SWT.NONE);  
-        button.setText("!点^.^我!");  
+        Button button = new Button(shell,SWT.NONE);  
+        button.setText("!点^.^我!"); 
         button.setLocation(70, 150);  
         button.setSize(100,25);  
   
@@ -27,13 +39,15 @@ public class Window {
         button.addSelectionListener(new SelectionListener() {  
             @Override  
             public void widgetSelected(SelectionEvent event) {  
-                MessageDialog.openInformation(shell, "你好", "青年");  
             }  
               
             @Override  
             public void widgetDefaultSelected(SelectionEvent e) {}  
         });  
-          
+        
+        Text text = new Text (shell, SWT.BORDER);
+        text.setText ("button0");
+		
         //4.设置显示  
         shell.layout();  
         shell.open();  
@@ -44,5 +58,18 @@ public class Window {
         }  
         display.dispose();  
     }  
+	
+	
+	
+	
+	void updateAllApps() throws Exception {
+		petrel.update_user_list();
+		techlog.update_user_list();
+		arcgis.update_user_list();
+		fme.update_user_list();
+		ofm.update_user_list();
+		edm.update_user_list();
+		ecrin.update_user_list();
+	}
 
 }
